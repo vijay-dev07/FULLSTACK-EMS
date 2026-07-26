b023be4ea938c1a63af78ea81eb2fbffabc5c847
vijay-dev07 <uk.kaithwas771@gmail.com>
2026-07-26 13:02:23 +0530

    backend completed

--- server/controllers/attendanceController.js
+++ server/controllers/attendanceController.js
@@ -3,6 +3,7 @@
 // Clock in/out for employee
 // POST /api/attendence
 
+import { inngest } from "../inngest/index.js";
 import Attendance from "../models/Attendance.js";
 import Employee from "../models/Employee.js";
 
@@ -33,6 +34,14 @@ export const clockInOut = async (req , res) => {
                 date:today,
                 checkIn:now,
                 status:isLate ? "LATE" : "PRESENT"
+            })
+
+            await inngest.send({
+                name:"employee/check-out",
+                data:{
+                    employeeId:employee._id,
+                    attendanceId:attendance._id,
+                }
             })
 
             return res.json({ success :true, type:"CHECK_IN", data:attendance});
--- server/server.js
+++ server/server.js
@@ -10,6 +10,8 @@ import attendanceRouter from "./routes/attendanceRoutes.js";
 import leaveRouter from "./routes/leaveRoutes.js";
 import payslipRouter from "./routes/payslipsRoutes.js";
 import dashboardRouter from "./routes/dashboardRoutes.js";
+import { serve } from "inngest/express";
+import { inngest, functions } from "./inngest/index.js"
 
 
 const app = express()
@@ -30,6 +32,8 @@ app.use("/api/attendance" , attendanceRouter)
 app.use("/api/leave" , leaveRouter)
 app.use("/api/payslips" , payslipRouter)
 app.use("/api/dashboard" , dashboardRouter)
+
+app.use("/api/inngest", serve({ client: inngest, functions }));
 
 await connectDB()
 app.listen(PORT , ()=> console.log(`Server is running on port ${PORT}`) )
